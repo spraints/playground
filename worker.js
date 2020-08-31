14a6b45ad73465d12d118a5d55f9f49393df7448
Matt Burke <spraints@gmail.com>
2020-08-31 11:19:52 -0400

    more messages

--- worker.js
+++ worker.js
@@ -2,9 +2,13 @@ var sw = self
 
 function handle(session, port, data) {
   if (data[1] == 'self-destruct') {
-    setTimeout(100, function() { sw.close() })
+    setTimeout(100, function() {
+      console.log('exited')
+      sw.close()
+    })
   }
 
+  console.log('number of subscriptions was ' + session.subscriptions.size)
   session.subscriptions.set(port, true)
   for (var sub of session.subscriptions.keys()) {
     sub.postMessage(data)
@@ -24,5 +28,6 @@ var x = doThings()
 
 sw.onconnect = function(e) {
   var port = e.ports[0]
+  x({target: port, data: ['???', 'connected']})
   port.onmessage = x
 }
