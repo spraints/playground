e14de58041fbf70469c6ec8dd78014dd64603538
Matt Burke <spraints@gmail.com>
2020-08-31 11:11:33 -0400

    use a map

--- worker.js
+++ worker.js
@@ -1,12 +1,12 @@
 function handle(session, port, data) {
-  session.subscriptions[port] = true
-  for (var sub of session.subscriptions) {
+  session.subscriptions.set(port, true)
+  for (var sub of session.subscriptions.keys()) {
     sub.postMessage(data)
   }
 }
 
 function doThings() {
-  var session = {subscriptions: {}}
+  var session = {subscriptions: new Map()}
   return function(e) {
     var port = e.target
     var data = e.data
